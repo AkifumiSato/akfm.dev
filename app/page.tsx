40bb2162273d765d68fbc32ff3dd157de84e662d
佐藤昭文 <akfm.sato@gmail.com>
2022-11-12 14:45:05 +0900

    top page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,51 @@
+import Link from 'next/link'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          # akfm.dev
-        </h1>
+      <div className={styles.contents}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            # akfm.dev
+          </h1>
+          <section className={styles.section}>
+            <h2 className={styles.sectionTitle}>## Name</h2>
+            <p>Akifumi Sato.</p>
+          </section>
+          <section className={styles.section}>
+            <h2 className={styles.sectionTitle}>## About</h2>
+            <p>Web application developer interested in Rust / Typescript.</p>
+          </section>
+          <section className={styles.section}>
+            <h2 className={styles.sectionTitle}>## Links</h2>
+            <ul>
+              <li>
+                <a href="https://github.com/AkifumiSato">github</a>
+              </li>
+              <li>
+                <a href="https://zenn.dev/akfm">zenn.dev</a>
+              </li>
+              <li>
+                <a href="https://twitter.com/akfm_sato">twitter</a>
+              </li>
+            </ul>
+          </section>
+        </main>
 
-        <section className={styles.section}>
-          <h2 className={styles.sectionTitle}>## About</h2>
-          <p>Akfiumi Sato.Web application developer interested in Rust / Typescript.</p>
-        </section>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-      </main>
+        <footer className={styles.footer}>
+          <p className={styles.copyright}>
+            ©︎akfm.dev 2022. Using&nbsp;
+            <a
+              href="https://www.google.com/intl/ja/policies/privacy/partners/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Google Analytics
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   )
 }
