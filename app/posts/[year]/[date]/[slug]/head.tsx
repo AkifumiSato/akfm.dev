74d96b833a889407e89d76dac4010be568da57ec
佐藤昭文 <akfm.sato@gmail.com>
2022-11-14 23:41:33 +0900

    titleが反映されない旨コメント

--- app/posts/[year]/[date]/[slug]/head.tsx
+++ app/posts/[year]/[date]/[slug]/head.tsx
@@ -1,7 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>akfm.dev</title>
+      {/* 以下バグのため適用されないが近いうちに解消されそう */}
+      {/* https://github.com/vercel/next.js/issues/42268 */}
+      <title>akfm.dev {"aaa"}</title>
     </>
   );
 }
