d54df238098ecabf3d5b2a6bd5be335f3a23421a
佐藤昭文 <akfm.sato@gmail.com>
2022-12-30 14:41:09 +0900

    postの並び順修正

--- TODO.md
+++ TODO.md
@@ -3,4 +3,4 @@
 - [ ] prism の追加
 - [ ] 過去記事を追加
 - [ ] deploy
-- [ ] md ファイルを GraphQL で取得するように修正
+- [ ] pager の実装
--- src/lib/server/posts/getAllPostsParams.ts
+++ src/lib/server/posts/getAllPostsParams.ts
@@ -2,19 +2,25 @@ import { glob } from 'glob'
 
 export function getAllPostsParams() {
   const files = glob.sync('posts/[0-9]*/[0-9]*/*.md')
-  return files.map((file) => {
-    // glob的に必ずmatch
-    const [_post, year, date, slugMd] = file.split('/')
-    const slug = slugMd.replace('.md', '')
-    if (year && date && slug) {
-      return {
-        params: {
-          year,
-          date,
-          slug,
-        },
+  return files
+    .map((file) => {
+      // glob的に必ずmatch
+      const [_post, year, date, slugMd] = file.split('/')
+      const slug = slugMd.replace('.md', '')
+      if (year && date && slug) {
+        return {
+          params: {
+            year,
+            date,
+            slug,
+          },
+        }
       }
-    }
-    throw new Error('/[year]/[date]/[slug]形式である必要があります')
-  })
+      throw new Error('/[year]/[date]/[slug]形式である必要があります')
+    })
+    .sort((prev, next) => {
+      const prevFullDate = `${prev.params.year}${prev.params.date}`
+      const nextFullDate = `${next.params.year}${next.params.date}`
+      return nextFullDate > prevFullDate ? 1 : -1
+    })
 }
